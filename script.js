9b077d0ddb864b4671bd9fa816d06414042a0092
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-07 08:35:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+
+// setTimeout(() => {
+// 	const element = document.querySelector('.holder');
+// 	for (let index = 0; index < 12; index++) {
+// 		element.innerHTML += `<div class="item">Item 4</div>`;
+// 	}
+   
+// },3000);
